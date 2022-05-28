0e54d2bb3ecec2b75ccfba11892dce12aafff60a
Nikit099 <nechit1337@gmail.com>
2022-05-28 15:05:37 +0300

    remove regected

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch, Redirect } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Navbar from "./components/assets/navbar";
 import AllCats from "./components/pages/allCats";
 import LikesCats from "./components/pages/likesCats";
@@ -9,11 +9,9 @@ function App() {
     <div className="App">
       <Navbar />
       <Switch>
-        <Route path="/" exact component={AllCats} />
+        <Route path="frontend-challenge/" exact component={AllCats} />
 
-        <Route path="/likesCats" component={LikesCats} />
-
-        <Redirect to="/" />
+        <Route path="frontend-challenge/likesCats" component={LikesCats} />
       </Switch>
     </div>
   );
